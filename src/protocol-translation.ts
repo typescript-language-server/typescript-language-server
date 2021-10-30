a46758418037e1d5228f7b3023e7f21d92bad860
Rafal Chlodnicki <rchl2k@gmail.com>
2021-10-30 23:29:49 +0200

    fix completion handling on windows

--- src/protocol-translation.ts
+++ src/protocol-translation.ts
@@ -10,12 +10,14 @@ import tsp from 'typescript/lib/protocol';
 import URI from 'vscode-uri';
 import { LspDocuments } from './document';
 
+const RE_PATHSEP_WINDOWS = /\\/g;
+
 export function uriToPath(stringUri: string): string | undefined {
     const uri = URI.parse(stringUri);
     if (uri.scheme !== 'file') {
         return undefined;
     }
-    return uri.fsPath;
+    return normalizeFsPath(uri.fsPath);
 }
 
 function parsePathOrUri(filepath: string): URI {
@@ -39,11 +41,19 @@ export function pathToUri(filepath: string, documents: LspDocuments | undefined)
  *
  * On systems other than Windows it should be an no-op.
  *
- * On Windows, an input path in a format like "c:/path/file.ts"
- * will be normalized to "c:\path\file.ts" (same as returned through URI.fsPath).
+ * On Windows, an input path in a format like "C:/path/file.ts"
+ * will be normalized to "c:/path/file.ts".
  */
-export function normalizeFileNameToFsPath(fileName: string): string {
-    return URI.file(fileName).fsPath;
+export function normalizePath(filePath: string): string {
+    const fsPath = URI.file(filePath).fsPath;
+    return normalizeFsPath(fsPath);
+}
+
+/**
+ * Normalizes the path obtained through the "fsPath" property of the URI module.
+ */
+export function normalizeFsPath(fsPath: string): string {
+    return fsPath.replace(RE_PATHSEP_WINDOWS, '/');
 }
 
 function currentVersion(filepath: string, documents: LspDocuments | undefined): number {
--- src/test-utils.ts
+++ src/test-utils.ts
@@ -8,7 +8,7 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as lsp from 'vscode-languageserver/node';
-import { normalizeFileNameToFsPath, pathToUri } from './protocol-translation';
+import { normalizePath, pathToUri } from './protocol-translation';
 import { LspServer } from './lsp-server';
 import { ConsoleLogger } from './logger';
 import { getTsserverExecutable } from './utils';
