dffc8165e9846af77ba14b7a559290a10234572b
Tom Payne <tom@tompayne.dev>
2023-01-22 18:01:56 +0000

    Swapped to client send message over stderr logger

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,6 @@ const config: Config = {
         // vscode-uri has buggy exports: https://github.com/microsoft/vscode-uri/pull/25
         'vscode-uri': 'vscode-uri/lib/esm/index.js',
     },
-    setupFiles: ['<rootDir>/setupTests.ts'],
     slowTestThreshold: 15,
     testTimeout: 20000,
     transform: {
