f827f2ea3f7c6b397b91fa1a808edd0ab9b0ec34
Rafal Chlodnicki <rchl2k@gmail.com>
2026-05-08 22:06:52 +0200

    enable coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
+        coverage: {
+            provider: 'v8',
+            reporter: 'text',
+        },
         testTimeout: 20000,
     },
 });
